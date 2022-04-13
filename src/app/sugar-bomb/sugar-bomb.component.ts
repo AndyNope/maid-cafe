075d2aabbada38170d762613d9c9f3b7731fe8b8
Andy Bui <andynopparat@gmail.com>
2022-04-13 09:23:39 +0200

    Sparkles added

--- src/app/sugar-bomb/sugar-bomb.component.ts
+++ src/app/sugar-bomb/sugar-bomb.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { SugarbombService } from '../shared/sugarbomb.service';
+
+declare function onStart(): any;
 
 @Component({
   selector: 'app-sugar-bomb',
   templateUrl: './sugar-bomb.component.html',
   styleUrls: ['./sugar-bomb.component.scss']
 })
-export class SugarBombComponent implements OnInit {
+export class SugarBombComponent implements OnInit, AfterViewInit {
 
 
   constructor(public sugarbombService: SugarbombService) { }
+
+  ngAfterViewInit(): void {
+    onStart();
+  }
 
   ngOnInit(): void {
   }
